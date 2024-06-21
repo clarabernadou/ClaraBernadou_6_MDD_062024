660b492a14208f0e0af23a45ad99b806d5aa2292
clarabernadou <clarabernadou56690@gmail.com>
2024-06-21 19:07:36 +0200

    [add] - dotenv config + connect back to database

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -2,12 +2,20 @@ package com.openclassrooms.mddapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MddApiApplication {
 
 	public static void main(String[] args) {
+	Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(MddApiApplication.class, args);
 	}
-
 }
