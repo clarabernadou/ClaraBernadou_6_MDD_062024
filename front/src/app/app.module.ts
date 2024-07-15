ad2f5fa9fe38d7cb395008232d43f01f1cc32c3e
clarabernadou <clarabernadou56690@gmail.com>
2024-07-15 21:32:12 +0200

    [misc] - remove old navbar + import navbar component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,9 +13,18 @@ import { RegisterComponent } from './pages/register/register.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { NavBarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, RegisterComponent, LoginComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ArticlesComponent,
+    NavBarComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,6 +35,7 @@ import { HttpClientModule } from '@angular/common/http';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    MatCardModule,
     HttpClientModule, 
   ],
   providers: [],
