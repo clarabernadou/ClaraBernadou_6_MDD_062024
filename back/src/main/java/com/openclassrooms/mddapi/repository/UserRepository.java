3efcad5bf9b7388cc710a14d7934e68345495ca7
clarabernadou <clarabernadou56690@gmail.com>
2024-07-16 21:44:00 +0200

    [add] - create user repository

---
+++ back/src/main/java/com/openclassrooms/mddapi/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.openclassrooms.mddapi.repository;
+
+import com.openclassrooms.mddapi.entity.Auth;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<Auth, Long> {
+    Optional<Auth> findById(Long id);
+}
