e345104b5465481bfccbf006c1cf4467b492a0b0
clarabernadou <clarabernadou56690@gmail.com>
2024-07-16 21:27:34 +0200

    [add] register/user's DTO

--- back/src/main/java/com/openclassrooms/mddapi/dto/RegisterDTO.java
+++ back/src/main/java/com/openclassrooms/mddapi/dto/RegisterDTO.java
@@ -4,9 +4,10 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
-public class AuthDTO {
+public class RegisterDTO {
     @NotBlank(message = "Email is mandatory")
     @Email(message = "Email should be valid")
     private String email;
@@ -15,5 +16,7 @@ public class AuthDTO {
     private String username;
 
     @NotBlank(message = "Password is mandatory")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$", 
+             message = "Password must contain at least one digit, one lowercase letter, one uppercase letter, one special character, and be at least 8 characters long")
     private String password;
 }
