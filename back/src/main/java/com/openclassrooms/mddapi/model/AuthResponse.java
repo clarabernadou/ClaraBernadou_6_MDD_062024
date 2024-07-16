ed9477c5560619776ee6af8ff512833938831657
clarabernadou <clarabernadou56690@gmail.com>
2024-07-16 21:42:15 +0200

    [edit] - update responses

--- back/src/main/java/com/openclassrooms/mddapi/model/AuthResponse.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/AuthResponse.java
@@ -5,11 +5,12 @@ import java.time.LocalDate;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import com.openclassrooms.mddapi.dto.AuthDTO;
+import com.openclassrooms.mddapi.dto.RegisterDTO;
+
 import lombok.Data;
 
 @Data
-public class AuthResponse extends AuthDTO {
+public class AuthResponse extends RegisterDTO {
 
     @JsonProperty("created_at")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy/MM/dd")
