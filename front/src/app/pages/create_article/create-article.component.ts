d83703a97a64c1617d93b130328d745f1c589be4
clarabernadou <clarabernadou56690@gmail.com>
2024-07-19 00:33:19 +0200

    [add] - create article page (OK)

---
+++ front/src/app/pages/create_article/create-article.component.ts
@@ -0,0 +1,111 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { catchError, forkJoin, map, merge, mergeMap, Observable, of } from 'rxjs';
+import { Article } from 'src/app/interfaces/article.interface';
+import { Theme } from 'src/app/interfaces/theme.interface';
+import { ArticleService } from 'src/app/services/article.service';
+import { BreakpointService } from 'src/app/services/breakpoint.service';
+import { ThemeService } from 'src/app/services/theme.service';
+
+@Component({
+  selector: 'app-create-article',
+  templateUrl: './create-article.component.html',
+  styleUrls: ['../../app.component.scss'],
+})
+export class CreateArticleComponent implements OnInit {
+  public isSmallScreen = false;
+  public isLargeScreen = false;
+  public loading = false;
+  public onError = false;
+  public errorMessage = '';
+  public submitted = false;
+  public themes: Observable<Theme[]> = this.getAllThemes();
+
+  public form = this.fb.group({
+    theme_id: [
+      '',
+      [
+        Validators.required,
+      ]
+    ],
+    title: [
+      '',
+      [
+        Validators.required
+      ]
+    ],
+    content: [
+      '',
+      [
+        Validators.required
+      ]
+    ]
+  });
+
+  constructor(
+    private breakpointService: BreakpointService,
+    private router: Router,
+    private fb: FormBuilder,
+    private articleService: ArticleService,
+    private themeService: ThemeService,
+  ) {}
+
+  ngOnInit() {
+    if (!localStorage.getItem('token')) this.router.navigate(['/login']);
+
+    this.breakpointService.isSmallScreen().subscribe(isSmall => {
+      this.isSmallScreen = isSmall;
+    });
+
+    this.breakpointService.isLargeScreen().subscribe(isLarge => {
+      this.isLargeScreen = isLarge;
+    });
+  }
+
+  public getAllThemes(): Observable<Theme[]> {
+    return this.themeService.getAllThemes().pipe(
+      mergeMap((themes: Theme[]) => {
+        const themeObservables = themes.map((theme: Theme) => {
+          return this.themeService.getTheme(theme.id!).pipe(
+            map((theme: Theme) => ({
+              ...theme,
+            }))
+          );
+        });
+        return forkJoin(themeObservables);
+      })
+    );
+  }
+
+  public submit(): void {
+    if (this.form.invalid) {
+      this.submitted = true;
+      this.form.markAllAsTouched();
+      return;
+    }
+
+    this.loading = true;
+    this.onError = false;
+    this.errorMessage = '';
+
+    const article: Article = {
+      theme_id: Number(this.form.get('theme_id')!.value!),
+      title: this.form.get('title')!.value!,
+      content: this.form.get('content')!.value!,
+    };
+
+    this.articleService.createArticle(article).subscribe(
+      (response) => {
+        this.loading = false;
+        this.router.navigate(['/articles']);
+      },
+      (error) => {
+        this.loading = false;
+        this.onError = true;
+        this.errorMessage = error.error.message;
+      }
+    );
+  }
+}
+
--- front/src/app/services/theme.service.ts
+++ front/src/app/services/theme.service.ts
@@ -1,40 +1,35 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthToken } from '../interfaces/authSession.interface';
+import { Login, Register } from '../interfaces/login.interface';
 import { environment } from '../../environments/environment';
 import { Article } from '../interfaces/article.interface';
+import { Theme } from '../interfaces/theme.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ArticleService {
-  private baseUrl = environment.baseUrl + '/auth/article';
+export class ThemeService {
+  private baseUrl = environment.baseUrl + '/auth/theme';
 
   constructor(private httpClient: HttpClient) {}
 
-  getAllArticles() {
+  getTheme(id: number) {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     });
-    return this.httpClient.get<Article[]>(`${this.baseUrl}/`, { headers });
+    return this.httpClient.get<Theme>(`${this.baseUrl}/${id}`, { headers });
   }
 
-  getArticleById(id: string) {
+  getAllThemes() {
     const token = localStorage.getItem('token');
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
       'Authorization': `Bearer ${token}`
     });
-    return this.httpClient.get<Article>(`${this.baseUrl}/${id}`, { headers });
-  }
-
-  createArticle(article: Article) {
-    const token = localStorage.getItem('token');
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${token}`
-    });
-    return this.httpClient.post<Article>(`${this.baseUrl}/`, article, { headers });
+    return this.httpClient.get<Theme[]>(`${this.baseUrl}/`, { headers });
   }
 }
