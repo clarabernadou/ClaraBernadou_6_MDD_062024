f6f87df51e7584ac8dc034b930888d31ebdcf662
clarabernadou <clarabernadou56690@gmail.com>
2024-07-25 15:12:23 +0200

    [misc] - add get all comments + update create comment + clean routes

--- back/src/main/java/com/openclassrooms/mddapi/controllers/CommentController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controllers/CommentController.java
@@ -1,6 +1,7 @@
 package com.openclassrooms.mddapi.controllers;
 
 import java.security.Principal;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
@@ -8,6 +9,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,20 +22,26 @@ import com.openclassrooms.mddapi.model.MessageResponse;
 import com.openclassrooms.mddapi.services.interfaces.CommentService;
 
 @RestController
-@RequestMapping("/api/auth/comment")
+@RequestMapping("/api/auth")
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
 
-    @PostMapping("/")
-    public ResponseEntity<?> createComment(@Valid @RequestBody CommentDTO commentDTO, Principal principalUser, BindingResult bindingResult) {
+    @GetMapping("/article/{articleId}/comment")
+    public ResponseEntity<?> getComments(@PathVariable Long articleId) {
+        return ResponseEntity.ok(commentService.getComments());
+    }
+
+    @PostMapping("/article/{articleId}/comment")
+    public ResponseEntity<?> createComment(@Valid @RequestBody CommentDTO commentDTO, @PathVariable Long articleId, Principal principalUser, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             String errors = bindingResult.getFieldErrors().stream()
                     .map(error -> error.getField() + ": " + error.getDefaultMessage())
                     .collect(Collectors.joining(", "));
             throw new UnauthorizedException(errors);
         }
-        return ResponseEntity.ok(new MessageResponse(commentService.createComment(commentDTO, principalUser).get()));
+        Optional<String> response = commentService.createComment(commentDTO, articleId, principalUser);
+        return ResponseEntity.ok(new MessageResponse(response.get()));
     }
 }
--- back/src/main/java/com/openclassrooms/mddapi/services/interfaces/CommentService.java
+++ back/src/main/java/com/openclassrooms/mddapi/services/interfaces/CommentService.java
@@ -1,16 +1,23 @@
 package com.openclassrooms.mddapi.services.interfaces;
 
-import java.security.Principal;
-import java.util.Optional;
-
 import com.openclassrooms.mddapi.dto.CommentDTO;
+
+import java.security.Principal;
+import java.util.*;
 
 public interface CommentService {
     /**
      * 
+     * @return
+     */
+    List<CommentDTO> getComments();
+
+    /**
+     * 
      * @param commentDTO
+     * @param articleId
      * @param principalUser
      * @return
      */
-    Optional<String>createComment(CommentDTO commentDTO, Principal principalUser);
+    Optional<String> createComment(CommentDTO commentDTO, Long articleId, Principal principalUser);
 }
