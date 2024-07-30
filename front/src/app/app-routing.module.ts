02a3b6951a0c2c15253753a3c5f3bfecf5108f07
clarabernadou <clarabernadou56690@gmail.com>
2024-07-30 17:07:47 +0200

    [add] - create routes + call components/pages

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ArticlesComponent } from './pages/articles/articles.component';
+import { ArticleDetailComponent } from './pages/articleDetail/article-detail.component';
+import { CreateArticleComponent } from './pages/createArticle/create-article.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ThemesComponent } from './pages/themes/themes.component';
+import { PageNotFoundComponent } from './pages/pageNotFound/page-not-found.component';
 
 // consider a guard combined with canLoad / canActivate route option
 // to manage unauthenticated user to access private routes
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: 'articles/detail/:id', component: ArticleDetailComponent },
+  { path: 'articles/create', component: CreateArticleComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'themes', component: ThemesComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: 'page-not-found' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
