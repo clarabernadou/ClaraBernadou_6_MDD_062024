97406d2e2e1c531de8942f63f0220206991600d2
clarabernadou <clarabernadou56690@gmail.com>
2024-09-05 16:39:26 +0200

    [fix] - update comments url bug + update ts files

--- front/src/app/services/comment.service.ts
+++ front/src/app/services/comment.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Comment } from '../interfaces/comment.interface';
 import { Observable, Subject } from 'rxjs';
 import { HttpOptionsService } from './httpOptions.service';
@@ -11,26 +11,25 @@ import { ApiService } from './api.service';
 export class CommentService {
   private baseAuthUrl: string;
 
-  private commentUpdatedSource: Subject<void> = new Subject<void>();
-  public commentUpdated$: Observable<void> = this.commentUpdatedSource.asObservable();
+  public commentCreated = new EventEmitter<void>();
 
   constructor(
     private httpClient: HttpClient,
     private httpOptionsService: HttpOptionsService,
     private apiService: ApiService,
   ) {
-    this.baseAuthUrl = this.apiService.getAuthUrl('/comment');
+    this.baseAuthUrl = this.apiService.getAuthUrl('/article');
   }
 
   public createComment(articleId: number, comment: Comment) {
-    return this.httpClient.post<Comment>(`${this.baseAuthUrl}/article/${articleId}/comment`, comment, this.httpOptionsService.getHttpOptions());
+    return this.httpClient.post<Comment>(`${this.baseAuthUrl}/${articleId}/comment`, comment, this.httpOptionsService.getHttpOptions());
   }
 
   public getComments(articleId: number) {
-    return this.httpClient.get<Comment[]>(`${this.baseAuthUrl}/article/${articleId}/comment`, this.httpOptionsService.getHttpOptions());
+    return this.httpClient.get<Comment[]>(`${this.baseAuthUrl}/${articleId}/comment`, this.httpOptionsService.getHttpOptions());
   }
 
   public notifyCommentUpdate(): void {
-    this.commentUpdatedSource.next();
+    this.commentCreated.emit();
   }
 }
